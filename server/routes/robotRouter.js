eeb73d63fdae05e80d4533f80f089d09df1bb4a5
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-18 20:44:07 +0100

    created first middleware for /robots route

--- server/routes/robotRouter.js
+++ server/routes/robotRouter.js
@@ -3,4 +3,9 @@ const express = require("express");
 
 const robotRouter = express.Router();
 
+router.get("/robots", async (req, res) => {
+  const robots = await Robot.find();
+  res.json({ robots });
+});
+
 module.exports = robotRouter;
