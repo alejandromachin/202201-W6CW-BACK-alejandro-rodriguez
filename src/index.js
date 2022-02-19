c381dbc59770a177507dadc7454f0556c7191575
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-19 09:35:46 +0100

    removed commented code from index js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
-// const connectToDatabase = require("./database/index");
+const connectToDatabase = require("./database/index");
 const runTheServer = require("./server/index");
 
 const port = process.env.SERVER_PORT || 4000;
-// const mongoPort = process.env.MONGO_CONECT;
+const mongoPort = process.env.MONGO_CONECT_PRODUCTION;
 
 (async () => {
   await runTheServer(port);
-  // await connectToDatabase(mongoPort);
+  await connectToDatabase(mongoPort);
 })();
