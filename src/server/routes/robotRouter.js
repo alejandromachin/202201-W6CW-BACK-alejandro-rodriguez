2e8f43d6a902a39929bbfb9b256e0dfe02728a19
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-20 11:56:03 +0100

    added test for this component

--- src/server/routes/robotRouter.js
+++ src/server/routes/robotRouter.js
@@ -1,9 +1,13 @@
 require("dotenv").config();
 const express = require("express");
-const { getAllRobots } = require("../controllers/robotsControler");
+const {
+  getAllRobots,
+  getRobotById,
+} = require("../controllers/robotsControler");
 
 const router = express.Router();
 
 router.get("/", getAllRobots);
+router.get("/:id", getRobotById);
 
 module.exports = router;
